7caa5072427eb95e28ad0d131842fc839c7e47c1
Zulfikar Zulfikar <0320200031@polman.astra.ac.id>
2021-05-27 10:25:27 +0800

    Jadii

--- QuizMahasiswa/ReportBarang019.Designer.cs
+++ QuizMahasiswa/ReportBarang019.Designer.cs
@@ -28,35 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.btnExcel = new System.Windows.Forms.Button();
             this.btnPDF = new System.Windows.Forms.Button();
-            this.quizAgitDataSet = new QuizMahasiswa.QuizAgitDataSet();
-            this.tblbarangBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.tbl_barangTableAdapter = new QuizMahasiswa.QuizAgitDataSetTableAdapters.tbl_barangTableAdapter();
-            this.idbarangDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.namabarangDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.hargaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.stokDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.namasupplierDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.quizAgitDataSet)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.tblbarangBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
-            this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.idbarangDataGridViewTextBoxColumn,
-            this.namabarangDataGridViewTextBoxColumn,
-            this.hargaDataGridViewTextBoxColumn,
-            this.stokDataGridViewTextBoxColumn,
-            this.namasupplierDataGridViewTextBoxColumn});
-            this.dataGridView1.DataSource = this.tblbarangBindingSource;
             this.dataGridView1.Location = new System.Drawing.Point(12, 57);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(544, 250);
@@ -91,50 +72,6 @@
             this.btnPDF.UseVisualStyleBackColor = true;
             this.btnPDF.Click += new System.EventHandler(this.btnPDF_Click);
             // 
-            // quizAgitDataSet
-            // 
-            this.quizAgitDataSet.DataSetName = "QuizAgitDataSet";
-            this.quizAgitDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // tblbarangBindingSource
-            // 
-            this.tblbarangBindingSource.DataMember = "tbl_barang";
-            this.tblbarangBindingSource.DataSource = this.quizAgitDataSet;
-            // 
-            // tbl_barangTableAdapter
-            // 
-            this.tbl_barangTableAdapter.ClearBeforeFill = true;
-            // 
-            // idbarangDataGridViewTextBoxColumn
-            // 
-            this.idbarangDataGridViewTextBoxColumn.DataPropertyName = "id_barang";
-            this.idbarangDataGridViewTextBoxColumn.HeaderText = "id_barang";
-            this.idbarangDataGridViewTextBoxColumn.Name = "idbarangDataGridViewTextBoxColumn";
-            // 
-            // namabarangDataGridViewTextBoxColumn
-            // 
-            this.namabarangDataGridViewTextBoxColumn.DataPropertyName = "nama_barang";
-            this.namabarangDataGridViewTextBoxColumn.HeaderText = "nama_barang";
-            this.namabarangDataGridViewTextBoxColumn.Name = "namabarangDataGridViewTextBoxColumn";
-            // 
-            // hargaDataGridViewTextBoxColumn
-            // 
-            this.hargaDataGridViewTextBoxColumn.DataPropertyName = "harga";
-            this.hargaDataGridViewTextBoxColumn.HeaderText = "harga";
-            this.hargaDataGridViewTextBoxColumn.Name = "hargaDataGridViewTextBoxColumn";
-            // 
-            // stokDataGridViewTextBoxColumn
-            // 
-            this.stokDataGridViewTextBoxColumn.DataPropertyName = "stok";
-            this.stokDataGridViewTextBoxColumn.HeaderText = "stok";
-            this.stokDataGridViewTextBoxColumn.Name = "stokDataGridViewTextBoxColumn";
-            // 
-            // namasupplierDataGridViewTextBoxColumn
-            // 
-            this.namasupplierDataGridViewTextBoxColumn.DataPropertyName = "nama_supplier";
-            this.namasupplierDataGridViewTextBoxColumn.HeaderText = "nama_supplier";
-            this.namasupplierDataGridViewTextBoxColumn.Name = "namasupplierDataGridViewTextBoxColumn";
-            // 
             // ReportBarang019
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -148,8 +85,6 @@
             this.Text = "ReportBarang019";
             this.Load += new System.EventHandler(this.ReportBarang019_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.quizAgitDataSet)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.tblbarangBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -160,13 +95,5 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnExcel;
         private System.Windows.Forms.Button btnPDF;
-        private QuizAgitDataSet quizAgitDataSet;
-        private System.Windows.Forms.BindingSource tblbarangBindingSource;
-        private QuizAgitDataSetTableAdapters.tbl_barangTableAdapter tbl_barangTableAdapter;
-        private System.Windows.Forms.DataGridViewTextBoxColumn idbarangDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn namabarangDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn hargaDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn stokDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn namasupplierDataGridViewTextBoxColumn;
     }
 }
--- QuizMahasiswa/ReportBarang019.cs
+++ QuizMahasiswa/ReportBarang019.cs
@@ -22,12 +22,18 @@ namespace QuizMahasiswa
 
         private void ReportBarang019_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'quizAgitDataSet.tbl_barang' table. You can move, or remove it, as needed.
-            this.tbl_barangTableAdapter.Fill(this.quizAgitDataSet.tbl_barang);
+            LoadData();
 
         }
 
-        public void exportintopdf(DataGridView dgw, string filename)
+        DatabaseZulDataContext db = new DatabaseZulDataContext();
+        void LoadData()
+        {
+            var data = from tb in db.tbl_barangs select tb;
+            dataGridView1.DataSource = data;
+        }
+
+        public void exportgridtopdf(DataGridView dgw, string filename)
         {
             BaseFont bf = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1250, BaseFont.EMBEDDED);
             PdfPTable pdftable = new PdfPTable(dgw.Columns.Count);
@@ -46,7 +52,7 @@ namespace QuizMahasiswa
                 pdftable.AddCell(cell);
             }
 
-            //Add DataRow
+            //add Data Rpw
             foreach (DataGridViewRow row in dgw.Rows)
             {
                 foreach (DataGridViewCell cell in row.Cells)
@@ -63,7 +69,7 @@ namespace QuizMahasiswa
             {
                 using (FileStream stream = new FileStream(savefiledialoge.FileName, FileMode.Create))
                 {
-                    Document pdfdoc = new Document(PageSize.A4, 10F, 10F, 10F, 0F);
+                    Document pdfdoc = new Document(PageSize.A4, 10f, 10f, 10f, 0f);
                     PdfWriter.GetInstance(pdfdoc, stream);
                     pdfdoc.Open();
                     pdfdoc.Add(pdftable);
@@ -73,18 +79,19 @@ namespace QuizMahasiswa
             }
         }
 
-        public void exportintoexcel(DataGridView dgw, string filename)
+        public void exportgridtoexcel(DataGridView dgw, string filename)
         {
             Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
             Microsoft.Office.Interop.Excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
             Microsoft.Office.Interop.Excel._Worksheet worksheet = null;
             worksheet = workbook.Sheets["Sheet1"];
             worksheet = workbook.ActiveSheet;
-            worksheet.Name = "DetailBarang";
+            worksheet.Name = "ProductDetail";
 
             for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
             {
                 worksheet.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
+
             }
 
             for (int i = 0; i < dataGridView1.Rows.Count; i++)
@@ -94,26 +101,28 @@ namespace QuizMahasiswa
                     worksheet.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
                 }
             }
-            var saveFileDialogeExcel = new SaveFileDialog();
-            saveFileDialogeExcel.FileName = "output excel";
-            saveFileDialogeExcel.DefaultExt = ".xlsx";
 
-            if (saveFileDialogeExcel.ShowDialog() == DialogResult.OK)
+            var savefiledialogExcel = new SaveFileDialog();
+            savefiledialogExcel.FileName = "Output Excel";
+            savefiledialogExcel.DefaultExt = ".xlsx";
+
+            if (savefiledialogExcel.ShowDialog() == DialogResult.OK)
             {
-                workbook.SaveAs(saveFileDialogeExcel.FileName, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
-
+                workbook.SaveAs(savefiledialogExcel.FileName, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, 
+                    Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
             }
+
             app.Quit();
         }
 
         private void btnPDF_Click(object sender, EventArgs e)
         {
-            exportintopdf(dataGridView1, "text");
+            exportgridtopdf(dataGridView1, "text");
         }
 
         private void btnExcel_Click(object sender, EventArgs e)
         {
-            exportintoexcel(dataGridView1, "text");
+            exportgridtoexcel(dataGridView1, "Excel");
         }
     }
 }
